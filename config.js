3aa57b3a8aebfdecfa7beac6580a46a28d63c005
wataru0220 <z2000wataru@gmail.com>
2026-06-14 14:40:47 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// ===== Firebase 設定 =====
+// オンライン対戦用。ローカル対戦だけならこのファイルは無くても動きます。
+// （これらの値はクライアントに公開される前提の識別子です。安全性は Realtime Database のルールで担保します）
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyACeVUzUS_lBw6YL95w8JkkUCNwN1ST_Gs",
+  authDomain: "osero-77308.firebaseapp.com",
+  databaseURL: "https://osero-77308-default-rtdb.firebaseio.com",
+  projectId: "osero-77308",
+  storageBucket: "osero-77308.firebasestorage.app",
+  messagingSenderId: "401249829167",
+  appId: "1:401249829167:web:c2649df6ff67f35958a1da",
+  measurementId: "G-VQPXQVRHDG"
+};
